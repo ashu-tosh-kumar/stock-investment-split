c453209fc1be3bf4e807f13e2a563679f7845c77
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2024-02-03 15:58:37 +0530

    added tests for file financial_instruments.py

--- test/mocks.py
+++ test/mocks.py
@@ -7,8 +7,8 @@ _financial_instrument_list = [
     {"identifier": "", "initial_investment": 50, "target_investment_ratio": 0.33},
     {"identifier": "", "initial_investment": 20, "target_investment_ratio": 0.34},
 ]
-_financial_instruments_models = [FinancialInstrument(**financial_instrument_dict) for financial_instrument_dict in _financial_instrument_list]
-DUMMY_FINANCIAL_INSTRUMENTS = FinancialInstruments(instruments=_financial_instruments_models)
+FINANCIAL_INSTRUMENTS_MODELS = [FinancialInstrument(**financial_instrument_dict) for financial_instrument_dict in _financial_instrument_list]
+DUMMY_FINANCIAL_INSTRUMENTS = FinancialInstruments(instruments=FINANCIAL_INSTRUMENTS_MODELS)
 
 
 class PropertyMock(Mock):
